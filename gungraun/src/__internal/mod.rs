199edd8724875cfc8fb8dd7cd71f80df21432dfa
gamma0987 <gamma0987@posteo.de>
2026-07-10 04:55:51 +0200

    Bump version 0.19.3 -> 0.19.4

--- gungraun/src/__internal/mod.rs
+++ gungraun/src/__internal/mod.rs
@@ -153,7 +153,7 @@ impl Runner {
         module_path: &str,
         bench_bin: String,
     ) -> Self {
-        const LIBRARY_VERSION: &str = "0.19.3";
+        const LIBRARY_VERSION: &str = "0.19.4";
 
         let mut cmd = std::process::Command::new(exe.unwrap_or("gungraun-runner"));
         cmd.arg(LIBRARY_VERSION);
